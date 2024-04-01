159fe28fd904097ce537d0ac8eca31800ee895fe
Theawmaster <105071602+Theawmaster@users.noreply.github.com>
2024-04-02 04:04:43 +0800

    Create Admin.java

---
+++ FOMS/src/entities/Admin.java
@@ -0,0 +1,108 @@
+package FOMSpackage.entities;
+
+/**
+ * The {@code Admin} class extends the {@code Staff} class and provides various administrative functionalities
+ * such as managing payments, staff, and branch operations within the system.
+ */
+public class Admin extends Staff {
+
+    /**
+     * Adds a payment to the system.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void addPayment() {
+        // TODO - implement Admin.addPayment
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Closes a branch of the organization.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void closeBranch() {
+        // TODO - implement Admin.closeBranch
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Edits the details of a manager in the system.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void editManager() {
+        // TODO - implement Admin.editManager
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Edits staff information in the system.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void editStaff() {
+        // TODO - implement Admin.editStaff
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Terminates the employment of a staff member.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void fireStaff() {
+        // TODO - implement Admin.fireStaff
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Hires a new staff member into the organization.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void hireStaff() {
+        // TODO - implement Admin.hireStaff
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Opens a new branch of the organization.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void openBranch() {
+        // TODO - implement Admin.openBranch
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Promotes a staff member within the organization.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void promoteStaff() {
+        // TODO - implement Admin.promoteStaff
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Removes a payment from the system.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void removePayment() {
+        // TODO - implement Admin.removePayment
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Transfers a staff member from one branch to another.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void transferStaff() {
+        // TODO - implement Admin.transferStaff
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Views all staff members in the system.
+     * This method is currently not implemented and will throw an {@code UnsupportedOperationException} if called.
+     */
+    public void viewAllStaff() {
+        // TODO - implement Admin.viewAllStaff
+        throw new UnsupportedOperationException();
+    }
+
+}
