80294cde3c32a2ccccea9152d9cac3c8a4fbfdd9
Theawmaster <105071602+Theawmaster@users.noreply.github.com>
2024-04-02 16:52:42 +0800

    Create iPaymentMethods.java

---
+++ FOMS/src/entities/iPaymentMethods.java
@@ -0,0 +1,12 @@
+package FOMSpackage.entities;
+
+/**
+ * The iPaymentMethods interface defines the methods related to payment modes.
+ */
+public interface iPaymentMethods {
+
+    /**
+     * Specifies the payment mode.
+     */
+    void paymentMode();
+}
