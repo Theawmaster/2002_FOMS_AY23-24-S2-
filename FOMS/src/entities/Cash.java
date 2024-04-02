5a60c0f1b3c2dbaa01bd81a2bcf9bba85cc9949c
Theawmaster <105071602+Theawmaster@users.noreply.github.com>
2024-04-02 16:54:03 +0800

    Create Cash.java

---
+++ FOMS/src/entities/Cash.java
@@ -0,0 +1,18 @@
+package FOMSpackage.entities;
+
+/**
+ * The Cash class represents a payment method using cash.
+ * It implements the iPaymentMethods interface.
+ */
+public class Cash implements iPaymentMethods {
+
+    /**
+     * Calculates the total amount to be paid in cash.
+     * 
+     * @throws UnsupportedOperationException if the method is not implemented.
+     */
+    public void totalAmount() {
+        // TODO - implement Cash.totalAmount
+        throw new UnsupportedOperationException();
+    }
+}
